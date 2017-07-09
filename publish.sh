cd7afdc8159eb0b0ce3d8154781a3d68e1c688e9
Dan Craig <Daniel.Craig@spacex.com>
2017-07-09 12:41:13 -0700

    Publish against whatever the last issue was

--- publish.sh
+++ publish.sh
@@ -5,7 +5,9 @@ mkdir -p issues
 cp broadsheet/templates/*.css issues/
 
 today=$(date +%Y-%m-%d)
-yesterday=$(date -v-1d +%Y-%m-%d)
+#yesterday=$(date -v-8d +%Y-%m-%d)
+yesterday=$(find issues/ -mtime +1 -type f | tail -n 1 | grep -o "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}")
+#yesterday="2017-02-04"
 
 yesterday_mtime=$(stat -f "%Sm" -n issues/$yesterday.html)
 
